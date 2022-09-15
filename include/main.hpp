102e2ab834682c4d5e34daec3d878bf7fb3a0d41
alph <alphikuuuu@gmail.com>
2022-09-15 22:28:20 +0500

    init commit

---
+++ include/main.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#ifndef MAIN_HPP
+#define MAIN_HPP
+
+#include <zmq.h>
+
+
+
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <main.hpp>
+#include <iostream>
+
+
+
+
+
+
+int main (void)
+{
+
+    std::cout << "hello new project" << std::endl;
+    return 0;
+
+}
